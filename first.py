ab0fce2a296f825e324edd2ccc80121531e185c7
Sania Nadaf <59957959+sanianadaf@users.noreply.github.com>
2024-03-08 06:27:06 +0000

    hello

--- first.py
+++ first.py
@@ -1,1 +1,27 @@
 print("hello")
+print(10)
+print(0o7)
+print(4.5e5)
+
+#complex numbers
+c1=3+6j
+c2=complex(5,9)
+print(c1)
+print(c2)
+print(c1.real)
+print(c2.imag)
+
+# to know the data type 
+x=5
+y=3.0
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+x=float(x)
+y=int(y)
+print(x)
+print(y)
+print(type(x))
+print(type(y))
