c0024f03a36666fae68ed6e2cf9b34de8827c835
Sania Nadaf <59957959+sanianadaf@users.noreply.github.com>
2024-03-11 04:28:49 +0000

    all

---
+++ 01string_formatting.py
@@ -0,0 +1,8 @@
+name="sania"
+place="bangalore"
+
+print(name+" is from "+place)
+print("%s is from %s " %(name, place))
+print("{} is from {}".format(name,place))
+print("{1} is from {0}".format(name,place))
+print(f"{name} is from {place}")
---
+++ 02strings.py
@@ -0,0 +1,12 @@
+name='sania'
+place="bangalore"
+company="""BGSW
+hitechcity madhapur hyderabad
+"""
+
+print(name,place,company)
+
+
+msg="it's a nice day"
+print(msg)
+
